b63eba21a5745435c536cd24f5f795ad35cdf9ab
Elliot Patros <empco@Elliots-iMac.local>
2016-03-18 11:41:46 -0700

    Initial Commit

---
+++ dyne/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  dyne
+//
+//  Created by Elliot Patros on 3/18/16.
+//  Copyright (c) 2016 Elliot Patros. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
